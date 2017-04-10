6b8a42d7b896f1618fb8b0e7ac4455d6f49ff832
Natalie Dodd <nataliedodd89@gmail.com>
2017-04-10 12:16:03 +0200

    wrote more tests to cover all component functions

--- app/assets/javascripts/test/test.js
+++ app/assets/javascripts/test/test.js
@@ -1,16 +1,24 @@
 var assert = require('assert');
 import React from 'react';
 import App from '../components/app';
-import configureMockStore from 'redux-mock-store'
+import configureMockStore from 'redux-mock-store';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import { Provider } from 'react-redux';
 
+import TopNav from '../containers/topnav';
 import ConnectedSurvey, { Survey } from '../containers/survey';
 import QuestionList from '../components/question_list';
-import QuestionsReducer from '../reducers/reducer_questions';
+import reducers from '../reducers';
 
 import { shallow, mount } from 'enzyme';
 import { expect } from 'chai';
 import redux from 'redux';
 import nock from 'nock';
+
+const createStoreWithMiddleware = applyMiddleware(
+  promise
+)(createStore);
 
 
 describe('App', function() {
@@ -21,22 +29,35 @@ describe('App', function() {
 
     describe('Survey', function() {
       it('should render the component called Question List', function() {
-        const wrapper = shallow(<Survey/>);
-        expect(wrapper.find(QuestionList)).to.have.length(1);
+        const wrapper = mount(
+          <Provider store={createStoreWithMiddleware(reducers)}>
+            <ConnectedSurvey />
+          </Provider>
+        );
+        if (wrapper.state.responseId) {
+          expect(wrapper.find(QuestionList)).to.have.length(1);
+        }
+
       });
-      it('should have an initial state of empty/null for all responses', function() {
-        const wrapper = shallow(<Survey/>);
-        console.log(wrapper.state().response);
-        expect(wrapper.state().response).to.deep.equal([]);
+      it('should have an initial state of null for responseId', function() {
+        const wrapper = mount(
+          <Provider store={createStoreWithMiddleware(reducers)}>
+            <ConnectedSurvey />
+          </Provider>
+        );
+        expect(wrapper.state.responseId).to.equal(undefined);
       });
-      it('should have a submit button that sends data to the API using the state', function() {
+      it('should have a form with submit button that sends responseID to the API', function() {
         const wrapper = shallow(<Survey/>);
         wrapper.find('button').simulate('click');
         // integrate nock
       });
-      it('should update the state of responses each time the Question component updates', function() {
+      it('should update the state of responseID after the form is submitted', function() {
         const wrapper = mount(<Survey/>);
         // integrate sinon and spy to test lifecycle
+      });
+      it('should validate that the input is not empty', function() {
+
       });
 
 
@@ -45,7 +66,7 @@ describe('App', function() {
           const wrapper = shallow(<Survey/>);
           expect(wrapper.find(SideNav)).to.have.length(1);
         });
-        it('Should render a list of questions', function() {
+        it('Should render a list of questions, equal to the length of the number of survey questions', function() {
 
         });
         it('Should have an initial state of 1 for active question', function() {
@@ -59,6 +80,15 @@ describe('App', function() {
 
         });
         it('Should update the state of active question if a back button is clicked', function() {
+
+        });
+        it('Should have an initial state of [] for responses', function() {
+
+        });
+        it('Should have a submit button that sends the responses to the API', function() {
+
+        });
+        it('Should show how many questions are unanswered, unless they are all answered', function() {
 
         });
 
@@ -84,13 +114,13 @@ describe('App', function() {
           it('Should have a back button, unless the question ID is 1', function() {
 
           });
-          it('Should have a next button, unless the question ID is === questions.length', function() {
+          it('Should have a next button', function() {
 
           });
-          it('Should have an initial active state of inactive, unless the question ID is 1', function() {
+          it('Should have an initial className of inactive, unless the question ID is 1', function() {
 
           });
-          it('Should have an initial active state of active, if the question ID is 1', function() {
+          it('Should have an initial className of active, if the question ID is 1', function() {
 
           });
           it('If it has a next button, onClick this should update the activeQuestion state + 1', function() {
@@ -99,9 +129,10 @@ describe('App', function() {
           it('If it has a back button, onClick this should update the activeQuestion state - 1', function() {
 
           });
-          it('If the active state is inActive, the fields should be disabled and the opacity of the component should be 0.5 ', function() {
+          it('If the active state is inActive, the fields should be disabled', function() {
 
           });
+
 
           describe('AnswerField', function() {
             it('Should render the correct type of input field depending on the question style', function() {
@@ -111,6 +142,23 @@ describe('App', function() {
 
             });
           });
+
+        });
+      });
+    });
+
+    describe('Results', function() {
+      it('Should fetch the results from the API', function() {
+
+      });
+      it('Should render a list of responses using the fetched data', function() {
+
+      });
+      describe('Response', function() {
+        it('After mounting, should fetch a list of questions and answers using the responseID that corresponds to the response', function() {
+
+        });
+        it('Should update the state of questions after fetching the data', function() {
 
         });
       });
